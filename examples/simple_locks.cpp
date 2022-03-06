e25f11d65dd05eb4bcd395f45c79929dfac3c9ee
Juan Camilo Morales <juan.morales@project44.com>
2022-03-06 17:16:51 -0500

    initial commit

---
+++ examples/simple_locks.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int current_count = 0;
+mutex critical_section;
+
+void increment(int n) {
+    critical_section.lock();
+    for (int i = 0; i < n; ++i) {
+        ++current_count;
+    }
+    critical_section.unlock();
+}
+
+int main() {
+    auto start_time = high_resolution_clock::now();
+
+    thread t1(increment, 99999);
+    thread t2(increment, 99999);
+
+    t1.join();
+    t2.join();
+
+    auto stop_time = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop_time - start_time);
+
+    cout << "Current count: " << current_count << endl;
+    cout << "Elapsed time: " << duration.count() << endl;
+    
+    return 0;
+}
